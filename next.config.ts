c09f616f12ba26fe3093c8c4c2b62984d5324a6e
joacos <joaquinj19@gmail.com>
2026-08-04 15:14:14 -0400

    Update next.config.ts for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["100.108.232.57", "192.168.1.35"],
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
